f244868c4f2a9060094d7f0849ec8072caf84c1b
ImamHello <147045100+ImamHello@users.noreply.github.com>
2023-11-15 16:17:05 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,32 +1,32 @@
 public class Dec2Hex {
+    try{
+        static String DecimalConvert(Object input) {
 
-    static String DecimalConvert(Object input) {
-        try{
-        String strconverted = input.toString();
-        int intconverted = Integer.parseInt(strconverted);
-
-        char ch[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-        int rem, num;
-        num = intconverted;
-        String hexadecimal = "";
-
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        if (num == 0) {
-            System.out.println("Hexadecimal representation is: 0");
-        } else {
-            while (num != 0) {
-                rem = num % 16;
-                hexadecimal = ch[rem] + hexadecimal;
-                num = num / 16;
+            String strconverted = input.toString();
+            int intconverted = Integer.parseInt(strconverted);
+    
+            char ch[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            int rem, num;
+            num = intconverted;
+            String hexadecimal = "";
+    
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+            if (num == 0) {
+                System.out.println("Hexadecimal representation is: 0");
+            } else {
+                while (num != 0) {
+                    rem = num % 16;
+                    hexadecimal = ch[rem] + hexadecimal;
+                    num = num / 16;
+                }
+                System.out.println("Hexadecimal representation is: " + hexadecimal);
             }
-            System.out.println("Hexadecimal representation is: " + hexadecimal);
-        }
-
-        return (hexadecimal);
-        }
-        catch(Exception e){
-            return("Ran into a problem");
-        }
+    
+            return (hexadecimal);
+            }
+    catch(Exception e){
+        return("Ran into a problem");
+    }
     }
 
     public static void main(String args[]) {
