11908d3edc44710538e5d33b711f7e0b9de811e5
ImamHello <147045100+ImamHello@users.noreply.github.com>
2023-11-15 17:03:49 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,21 +1,32 @@
+import static org.junit.Assert.assertEquals;
+
 public class Dec2Hex {
 
+    public String DecimalConvert() {
+        System.out.println("Ran into a problem");
+        return ("Ran into a problem");
+    }
 
-        public String DecimalConvert() { 
-        System.out.println("No Input Required");
-        return("Ran into a proble"); 
-        } 
-        static String DecimalConvert(Object input) {
-        try{
+    static String DecimalConvert(Object input) {
+        try {
+            boolean ff;
+            String f = "";
+            System.out.println(input.getClass().getSimpleName());
+            System.out.println(f.getClass().getSimpleName());
+            if (input.getClass().getSimpleName() == f.getClass().getSimpleName()) {
+                System.out.println("bark bark bark ohwous");
+            }
+            ff = assertEquals("Testing Dec2Hex with a value of 20 answer should be 14", f.getClass().getSimpleName(),
+                    input.getClass().getSimpleName());
 
             String strconverted = input.toString();
             int intconverted = Integer.parseInt(strconverted);
-    
+
             char ch[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
             int rem, num;
             num = intconverted;
             String hexadecimal = "";
-    
+
             System.out.println("Converting the Decimal Value " + num + " to Hex...");
             if (num == 0) {
                 System.out.println("Hexadecimal representation is: 0");
@@ -27,17 +38,16 @@ public class Dec2Hex {
                 }
                 System.out.println("Hexadecimal representation is: " + hexadecimal);
             }
-    
+
             return (hexadecimal);
-            }
-    catch(Exception e){
-        return("Ran into a problem");
-    }
+        } catch (Exception e) {
+            return ("Ran into a problem");
+        }
     }
 
     public static void main(String args[]) {
         DecimalConvert(20);
+        DecimalConvert("20");
 
     }
 }
-
