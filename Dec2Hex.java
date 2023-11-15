4a2b0d000fc8ff28e737e2a1642ccd3ab4b7723b
“ImamHello” <Cameron.stuart11@virginmedia.com>
2023-11-15 19:36:03 +0000

    adding comments

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,17 +1,21 @@
 public class Dec2Hex {
 
+// method with no inputs to  returns string and dispalys error message to console
     public String DecimalConvert() {
         System.out.println("No input Please enter an integer");
         return ("Ran into a problem");
     }
 
+// creats a funtion that retunrs a string and takes in an object
     static String DecimalConvert(Object input) {
         try {
+// checks to see the type of the passed in opbejct and error if it isn't some comoun types
             if (input instanceof String || input instanceof Double || input instanceof Float ) {
                 System.out.println("You have entered a non integer");
                 return ("isstring");
-
+// prints error and returns string 
             } else {
+// if it wasn't a none intiger then converts the varables so that into converted can set up num later
                 String strconverted = input.toString();
                 int intconverted = Integer.parseInt(strconverted);
 
@@ -19,7 +23,7 @@ public class Dec2Hex {
                 int rem, num;
                 num = intconverted;
                 String hexadecimal = "";
-
+// setting up the char array and varables for the looping and the retunr hex sring
                 System.out.println("Converting the Decimal Value " + num + " to Hex...");
                 if (num == 0) {
                     System.out.println("Hexadecimal representation is: 0");
@@ -31,13 +35,14 @@ public class Dec2Hex {
                     }
                     System.out.println("Hexadecimal representation is: " + hexadecimal);
                 }
+// loops throush the numbers while its not 0 and does the converting to hexidecimal and prints the hex value
 
                 return (hexadecimal);
-
+// returns the string 
             }
 
         } catch (Exception e) {
             return ("Ran into a problem");
-        }
+        }// encase of error retunrs ran into a prroblem text
     }
 }
