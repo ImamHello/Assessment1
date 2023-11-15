28903c9f7904cfafe8dd5a5080427e1ea1a365e5
ImamHello <147045100+ImamHello@users.noreply.github.com>
2023-11-15 17:16:06 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -11,33 +11,35 @@ public class Dec2Hex {
 
             if (input instanceof String) {
                 System.out.println("You have entered a string Enter a number next time");
-                return("isstring");
+                return ("isstring");
 
-            }
-
-            String strconverted = input.toString();
-            int intconverted = Integer.parseInt(strconverted);
-
-            char ch[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-            int rem, num;
-            num = intconverted;
-            String hexadecimal = "";
-
-            System.out.println("Converting the Decimal Value " + num + " to Hex...");
-            if (num == 0) {
-                System.out.println("Hexadecimal representation is: 0");
             } else {
-                while (num != 0) {
-                    rem = num % 16;
-                    hexadecimal = ch[rem] + hexadecimal;
-                    num = num / 16;
+                String strconverted = input.toString();
+                int intconverted = Integer.parseInt(strconverted);
+
+                char ch[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+                int rem, num;
+                num = intconverted;
+                String hexadecimal = "";
+
+                System.out.println("Converting the Decimal Value " + num + " to Hex...");
+                if (num == 0) {
+                    System.out.println("Hexadecimal representation is: 0");
+                } else {
+                    while (num != 0) {
+                        rem = num % 16;
+                        hexadecimal = ch[rem] + hexadecimal;
+                        num = num / 16;
+                    }
+                    System.out.println("Hexadecimal representation is: " + hexadecimal);
                 }
-                System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+                return (hexadecimal);
+
             }
 
-            return (hexadecimal);
         } catch (Exception e) {
             return ("Ran into a problem");
         }
-    }    
+    }
 }
